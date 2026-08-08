5d062eb5295207cbefecb27babbff990e950dce6
egorrachkov373-stack <egorrachkov373@gmail.com>
2026-08-08 10:53:40 +0300

    sfsfsf

---
+++ server.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Response, Request, Form
+from fastapi.templating import Jinja2Templates
+import json
+from starlette.responses import HTMLResponse
+
+templates = Jinja2Templates(directory='templates')
+app = FastAPI()
+
+
+@app.post('/login')
+async def registration(request: Request):
