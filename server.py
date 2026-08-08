fabfa2abae0e60d71af7f7a86909935476125e0c
egorrachkov373-stack <egorrachkov373@gmail.com>
2026-08-08 11:55:41 +0300

    xfbgc

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, Request, Form
+from fastapi import FastAPI, Response, Request, Form, HTTPException
 from fastapi.templating import Jinja2Templates
 import json
 from starlette.responses import HTMLResponse
@@ -9,12 +9,14 @@ USERS_JSON = 'users.json'
 ROOMS_JSON = 'rooms.json'
 
 
-@app.post('/login')
-async def login(request: Request, mail: str = Form(...), password: str = Form(...)):
+@app.get("/login", response_class=HTMLResponse)
+async def get_login_page(request: Request):
+    return templates.TemplateResponse(request, 'login.html')
+
+
+@app.post("/login")
+async def do_login(response: Response, mail: str = Form(...), password: str = Form(...)):
     with open(USERS_JSON, "r", encoding="utf-8") as f:
         users = json.load(f)
-    if mail not in users:
-        
-
-
-
+    if mail not in users or users[mail]["password"] != password:
+        raise HTTPException(status_code=400, detail="Неверный email или пароль")
