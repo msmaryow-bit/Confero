3b5fb3aa9cf9aa51d068decb36f918a857356162
egorrachkov373-stack <egorrachkov373@gmail.com>
2026-08-08 14:05:19 +0300

    смиттома

--- server.py
+++ server.py
@@ -1,21 +1,46 @@
-from fastapi import FastAPI, Response, Request, Form, HTTPException
+import os
+from fastapi import FastAPI, Response, Request, Form
 from fastapi.templating import Jinja2Templates
 import json
 from starlette.responses import HTMLResponse
 
 templates = Jinja2Templates(directory='templates')
 app = FastAPI()
-USERS_JSON = 'users.json'
-ROOMS_JSON = 'rooms.json'
+
+sl = {}
+
+
+@app.get("/register", response_class=HTMLResponse)
+async def show_root(request: Request):
+    return templates.TemplateResponse(request, "register.html")
 
 
 @app.get("/login", response_class=HTMLResponse)
-async def get_login_page(request: Request):
-    return templates.TemplateResponse(request, 'login.html')
+async def show_root1(request: Request):
+    return templates.TemplateResponse(request, "login.html")
 
 
-@app.post("/login")
-async def do_login(response: Response, mail: str = Form(...), password: str = Form(...)):
-    with open(USERS_JSON, "r", encoding="utf-8") as f:
-        users = json.load(f)
+@app.get("/main", response_class=HTMLResponse)
+async def show_root2(request: Request):
+    return templates.TemplateResponse(request, "main.html")
 
+
+@app.post('/register', response_class=HTMLResponse)
+async def register(request: Request, name: str = Form(...),
+                   email: str = Form(...),
+                   password: str = Form(...),
+                   organization: str = Form(...)):
+    with open('users.json', 'w', encoding='utf-8') as users:
+        sl[email] = {'name': name, 'password': password, 'organization': organization}
+        users.write(json.dumps(sl, indent=4, ensure_ascii=False))
+    return templates.TemplateResponse(request, "main.html")
+
+
+@app.on_event('startup')
+async def startup():
+    global sl
+    if os.path.exists('users.json'):
+        with open('users.json', 'r', encoding='utf-8') as users:
+            sl = json.loads(users.read())
+    else:
+        sl = {}
